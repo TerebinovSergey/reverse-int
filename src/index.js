58fb343092effdcdeabb590b52c6ba6e1d4ef94f
TerebinovSergey <neznacomec94@gmail.com>
2022-08-08 19:40:40 +0400

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n *= -1;
+    arr = n.toString().split('').reverse();
+    
+    while (true) {
+        if (arr[0] == '0') {
+            arr.shift();
+        } else {
+            break;
+        }
+    }
+    
+    return arr.join('');
 }
